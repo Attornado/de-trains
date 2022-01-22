ce53b74152c5189291f9ba88df92f0bf81c65904
Attornado <a.terlizzi2@studenti.unisa.it>
2022-01-22 19:27:56 +0100

    Small fixes

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,6 @@
-var TutorialToken = artifacts.require("TutorialToken");
-var TutorialToken_AC = artifacts.require("TutorialToken_AC");
+var TicketStore = artifacts.require("TicketStore");
 
 
 module.exports = function(deployer) {
-   deployer.deploy(TutorialToken, 'SimpleToken', 'SIM', '10000000000000000000000');
-   deployer.deploy(TutorialToken_AC, 'SimpleToken_AC', 'SIM_AC', '10000000000000000000000');
+   deployer.deploy(TicketStore);
 }
