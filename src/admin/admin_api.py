5249fb65d4100c5c2eda57ab89b352c1a17734a9
Attornado <73835871+attornado@users.noreply.github.com>
2022-07-28 13:04:48 +0200

    Handled ticked elimination for admin.
    Signed-off-by: Attornado <73835871+Attornado@users.noreply.github.com>

--- src/admin/admin_api.py
+++ src/admin/admin_api.py
@@ -1,4 +1,4 @@
-from flask import jsonify, request, render_template
+from flask import jsonify, request, render_template, redirect
 from web3 import exceptions
 import web3
 from src.contract_setup import web3, contract
@@ -18,6 +18,16 @@ def show_admin_page():
         return render_template('./admin.html')
     else:
         return jsonify({"message": "You are not an admin!"}), 400
+
+
+@ADMIN_API.route("/admin/update_page")
+@ADMIN_API.route("/admin/insert_page")
+def show_product_form():
+    modify = request.args.get('modify')
+    insert = request.args.get('insert')
+    if contract.functions.isAdmin(account=web3.eth.defaultAccount).call():
+        contract.functions.isAdmin(account=web3.eth.defaultAccount).transact()  # Notarization
+        return render_template('./product_form.html', insert=insert, modify=modify)
 
 
 @ADMIN_API.route("/admin/register_admin", methods=["GET"])
@@ -155,72 +165,16 @@ def update_ticket_api():
 @ADMIN_API.route("/admin/delete_ticket", methods=["GET"])
 def delete_ticket_api():
     db_id = int(request.args.get('db_id'))
+    reload = request.args.get('reload')
 
     if contract.functions.isAdmin(account=web3.eth.defaultAccount).call():
         contract.functions.isAdmin(account=web3.eth.defaultAccount).transact()  # Notarization
         if delete_ticket(db_id):
-            return jsonify({"message": f"Ticket with id: {db_id} deleted successfully!"}), 200
+            if reload is not None and reload != "":
+                return redirect("/products", code=302)
+            else:
+                return jsonify({"message": f"Ticket with id: {db_id} deleted successfully!"}), 200
         else:
             return jsonify({"message": f"Ticket with id {db_id} not deleted because it doesn't exist!"}), 400
     else:
         return jsonify({"message": "Ticket delete failed: you are not an admin!"}), 400
-
-
-'''
-@ADMIN_API.route("/admin/remove_admin", methods=["GET"])
-def remove_admin():
-    if not set_account_flag:
-        return jsonify({"message": "You are not logged in!"}), 400
-    address = request.args.get('address')
-    if contract.functions.isAdmin(account=web3.eth.defaultAccount).call():
-        contract.functions.isAdmin(account=web3.eth.defaultAccount).transact()  # Notarization
-
-        try:
-            contract.functions.removeAdminRole(to=address).call()
-            contract.functions.removeAdminRole(to=address).transact()  # Notarization
-        except exceptions.SolidityError as e:
-            return jsonify({"message": "Admin role assignation failed: " + str(e)}), 400
-
-        return jsonify({"message": "Admin role remove successfully to address: " + str(address)}), 200
-    else:
-        return jsonify({"message": "Admin role renounce failed: you are not an admin!"}), 400
-
-
-@ADMIN_API.route("/admin/remove_ticket_usage_setter", methods=["GET"])
-def remove_ticket_usage_setter():
-    if not set_account_flag:
-        return jsonify({"message": "You are not logged in!"}), 400
-    address = request.args.get('address')
-    if contract.functions.isUsageSetter(account=web3.eth.defaultAccount).call():
-        contract.functions.isUsageSetter(account=web3.eth.defaultAccount).transact()  # Notarization
-
-        try:
-            contract.functions.removeUsageSetterRole(to=address).call()
-            contract.functions.removeUsageSetterRole(to=address).transact()  # Notarization
-        except exceptions.SolidityError as e:
-            return jsonify({"message": "Usage setter role assignation failed: " + str(e)}), 400
-
-        return jsonify({"message": "Usage setter role removed successfully to address: " + str(address)}), 200
-    else:
-        return jsonify({
-            "message": "Usage setter role renounce failed: you are not an admin nor a usage setter!"
-        }), 400
-
-
-@ADMIN_API.route("/admin/transfer_founds", methods=["GET"])
-def transfer_founds():
-    address = request.args.get('transfer_address')
-    if contract.functions.isAdmin(account=web3.eth.defaultAccount).call():
-        contract.functions.isAdmin(account=web3.eth.defaultAccount).transact()
-
-        try:
-            contract.functions.removeUsageSetterRole(to=address).call()
-            contract.functions.removeUsageSetterRole(to=address).transact()  # Notarization
-        except exceptions.SolidityError as e:
-            return jsonify({"message": "Usage setter role assignation failed: " + str(e)}), 400
-
-    else:
-        return jsonify({
-            "message": "Usage setter role renounce failed: you are not an admin nor a usage setter!"
-        }), 400
-'''
